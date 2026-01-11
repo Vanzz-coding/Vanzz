3ff2e8bcb2e0534e53504c253b58bc4f7b3073c3
Vanzz-coding <evantiono03@gmail.com>
2026-01-11 16:20:47 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openCart() {
+  const price = document.getElementById("paket").value;
+  document.getElementById("total").innerText = price;
+  document.getElementById("cart").style.display = "block";
+}
+
+function closeCart() {
+  document.getElementById("cart").style.display = "none";
+}
