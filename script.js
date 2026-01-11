3a97ca8aca6ac779421bad577b49c3a85265a83a
Vanzz-coding <evantiono03@gmail.com>
2026-01-11 16:12:30 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function addCart() {
-  let price = document.getElementById("paket").value;
-  document.getElementById("total").innerText = price;
-  document.getElementById("cart").style.display = "block";
-}
-
-function closeCart() {
-  document.getElementById("cart").style.display = "none";
-}
