e4142ceaca40414e2d7baa2a7ecbf562b1c829b6
Vanzz-coding <evantiono03@gmail.com>
2026-04-18 20:05:11 +0700

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// ambil reff dari URL
 const urlParams = new URLSearchParams(window.location.search)
-const reff = urlParams.get("reffid") || "tidak ada"
+const reff = urlParams.get("reffid") || "-"
 
 function pilih(paket, harga) {
-  document.getElementById("paket").value = paket;
-  document.getElementById("harga").value = "Rp " + harga;
-  document.getElementById("form").scrollIntoView({ behavior: "smooth" });
+  document.getElementById("paket").value = paket
+  document.getElementById("harga").value = "Rp " + harga
+  document.getElementById("form").scrollIntoView({ behavior: "smooth" })
 }
 
 function kirim() {
-  let nama = document.getElementById("nama").value;
-  let wa = document.getElementById("wa").value;
-  let paket = document.getElementById("paket").value;
-  let harga = document.getElementById("harga").value;
+  let nama = document.getElementById("nama").value
+  let wa = document.getElementById("wa").value
+  let paket = document.getElementById("paket").value
+  let harga = document.getElementById("harga").value
 
   if (!nama || !wa || !paket) {
-    alert("Isi semua data!");
-    return;
+    alert("Lengkapi data dulu!")
+    return
   }
 
-  let admin = "62XXXXXXXXXX"; // GANTI NOMOR KAMU
+  let admin = "62XXXXXXXXXX"
 
   let pesan = `Halo Admin, saya ingin order panel
 
@@ -27,9 +26,7 @@ Nama: ${nama}
 No WA: ${wa}
 Paket: ${paket}
 Harga: ${harga}
-Referral: ${reff}
+Referral: ${reff}`
 
-Mohon diproses 🙏`;
-
-  window.open(`https://wa.me/${admin}?text=${encodeURIComponent(pesan)}`);
+  window.open(`https://wa.me/${admin}?text=${encodeURIComponent(pesan)}`)
 }
