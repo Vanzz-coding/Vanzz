1a07e7310e5d03cbd7a5b9b98dfd2498e2a0e0f3
Vanzz-coding <evantiono03@gmail.com>
2026-04-18 20:40:19 +0700

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const reff = new URLSearchParams(location.search).get("reffid") || "-"
 document.getElementById("reff").innerText = reff
 
-function pilih(p,h){
-  paket.value=p
-  harga.value="Rp "+h
+function pilih(el,p,h){
+  document.getElementById("paket").value=p
+  document.getElementById("harga").value="Rp "+h
+
   document.querySelectorAll(".ram-box").forEach(e=>e.classList.remove("active"))
-  event.currentTarget.classList.add("active")
+  el.classList.add("active")
+
   toast("Pilih "+p)
 }
 
-function toast(t){
-  let x=document.getElementById("toast")
-  x.innerText=t
-  x.style.display="block"
-  setTimeout(()=>x.style.display="none",2000)
+function toast(msg){
+  let t=document.getElementById("toast")
+  t.innerText=msg
+  t.style.display="block"
+  setTimeout(()=>t.style.display="none",2000)
 }
 
 function kirim(){
-  let admin="62XXXXXXXXXX"
+  let admin="6285695344508"
+
   let text=`Order Panel
 Nama:${nama.value}
 WA:${wa.value}
