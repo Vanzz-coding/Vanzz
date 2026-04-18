065cc72e5b32ecd5b50bb3ced0a47a4517f98d4a
Vanzz-coding <evantiono03@gmail.com>
2026-04-18 20:36:12 +0700

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 const reff = new URLSearchParams(location.search).get("reffid") || "-"
 document.getElementById("reff").innerText = reff
 
-function pilih(paket,harga){
-  paketInput.value = paket
-  hargaInput.value = "Rp "+harga
-  toast("Pilih "+paket)
+function pilih(p,h){
+  paket.value=p
+  harga.value="Rp "+h
+  document.querySelectorAll(".ram-box").forEach(e=>e.classList.remove("active"))
+  event.currentTarget.classList.add("active")
+  toast("Pilih "+p)
 }
 
-function toast(msg){
-  let t=document.getElementById("toast")
-  t.innerText=msg
-  t.style.display="block"
-  setTimeout(()=>t.style.display="none",2000)
+function toast(t){
+  let x=document.getElementById("toast")
+  x.innerText=t
+  x.style.display="block"
+  setTimeout(()=>x.style.display="none",2000)
 }
 
 function kirim(){
-  let nama=nama.value
-  let wa=wa.value
-  let paket=paketInput.value
-  let harga=hargaInput.value
-
-  let admin="6285695344508"
-
+  let admin="62XXXXXXXXXX"
   let text=`Order Panel
-Nama:${nama}
-WA:${wa}
-Paket:${paket}
-Harga:${harga}
+Nama:${nama.value}
+WA:${wa.value}
+Paket:${paket.value}
+Harga:${harga.value}
 Reff:${reff}`
 
   window.open(`https://wa.me/${admin}?text=${encodeURIComponent(text)}`)
